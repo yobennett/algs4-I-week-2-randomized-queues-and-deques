f1e1e2e7db0b7ffb9ed1eb89a4cee53946022b6c
Bennett <bennett@yobennett.com>
2015-02-15 00:35:10 -0800

    subset client

---
+++ src/Subset.java
@@ -0,0 +1,22 @@
+public class Subset {
+
+    public static void main(String[] args) {
+
+        int k = Integer.parseInt(args[0]);
+
+        RandomizedQueue<String> q = new RandomizedQueue<String>();
+
+        while(!StdIn.isEmpty()) {
+            q.enqueue(StdIn.readString());
+        }
+
+        for (String s : q) {
+            if (k <= 0) {
+                break;
+            }
+            System.out.println(s);
+            k -= 1;
+        }
+
+    }
+}
